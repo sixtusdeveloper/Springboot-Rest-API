a0d325a443c19f77caff4d2cbfa8be816b3bce95
sixtusdeveloper <sixtusushrey@gmail.com>
2025-01-15 21:19:20 +0100

    created a springboot rest api that returns a list in a json format

--- src/main/java/net/sixtusdev/springboot/controller/StudentController.java
+++ src/main/java/net/sixtusdev/springboot/controller/StudentController.java
@@ -2,6 +2,9 @@ package net.sixtusdev.springboot.controller;
 
 import net.sixtusdev.springboot.bean.Student;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +22,20 @@ public class StudentController {
 
         );
         return student;
+    }
+
+    // HTTP GET Request
+    // http://localhost:8080/students to access this method
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>(); // Create an ArrayList object
+        students.add(new Student(1, "John", "Doe"));
+        students.add(new Student(2, "Jane", "Doe"));
+        students.add(new Student(3, "John", "Smith"));
+        students.add(new Student(4, "Jane", "Smith"));
+
+        return students;
     }
 
 }
